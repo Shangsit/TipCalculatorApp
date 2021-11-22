a856276e1d29186a3f09a6427538948f842cf37d
Shangsit <shangsitnath01@gmail.com>
2021-11-23 00:23:51 +0530

    Finished Html and CSS portions

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// just for testing
+console.log("Hello World!");
+
+
+
+
+// Red Error 
+if(input[type="number"] == 0){
+    document.getElementsByClassName("redError").style.display = "inline";
+}
