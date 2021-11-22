276936ff7ad75902079573dae52e23097227280c
Shangsit <shangsitnath01@gmail.com>
2021-11-23 01:04:57 +0530

    js for red error is done

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,24 @@ console.log("Hello World!");
 
 
 // Red Error 
-if(input[type="number"] == 0){
-    document.getElementsByClassName("redError").style.display = "inline";
+document.getElementById("nop").onclick = function(){
+    const nop = document.getElementById("nop");
+    const red = document.getElementById("redError");
+  
+    if(nop.value == 0){
+        red.style.display = "inline";
+        nop.style.border = "2px solid red";
+        console.log("Error");
+    }else{
+        red.style.display = "none";
+        nop.style.border = "2px solid hsl(172, 67%, 45%)";
+    }
+}
+
+document.getElementById("custom").onclick = function(){
+    const custom = document.getElementById("custom");
+    if(custom.innerText == "Custom"){
+        console.log("checking");
+        custom.innerText = "HERO";
+    } 
 }
